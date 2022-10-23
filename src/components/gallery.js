e3c7759fdd586e00de1e296dc87c5008f326c4fd
zzulanas <zzulanas@gmail.com>
2022-10-23 15:16:44 -0400

    updated things

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -14,7 +14,6 @@ export default function Gallery() {
     }
   }
 `)
-console.log(data.contentfulGallery.images)
   return (
     <Section>
       <Container>
--- src/pages/{Page.slug}.js
+++ src/pages/{Page.slug}.js
@@ -38,6 +38,11 @@ export const query = graphql`
         url
         gatsbyImageData
       }
+      gallery {
+        images {
+          gatsbyImageData
+        }
+      }
       html
     }
   }
